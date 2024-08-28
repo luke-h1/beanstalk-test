d70d469b482232776092833e3a44b35e3d06981f
luke-h1 <luke.howsam@yahoo.com>
2024-08-28 13:24:56 +0100

    remove tmpl dep

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -4,23 +4,28 @@ resource "aws_s3_bucket" "ebs" {
   bucket = "${var.app_name}-${random_pet.ebs_bucket_name.id}"
 }
 
-data "template_file" "ebs_config" {
-  template = file("${path.module}/Dockerrun.aws.json.tpl")
-  vars = {
-    image_name = "${aws_ecr_repository.app.repository_url}:latest"
-  }
-}
 
-resource "local_file" "ebs_config" {
-  content  = data.template_file.ebs_config.rendered
-  filename = "${path.module}/Dockerrun.aws.json"
-}
 
 resource "aws_s3_object" "ebs_deployment" {
-  depends_on = [local_file.ebs_config]
   bucket     = aws_s3_bucket.ebs.id
   key        = "Dockerrun.aws.json"
-  source     = "${path.module}/Dockerrun.aws.json"
+  source = jsonencode({
+    "AWSEBDockerrunVersion" : "1",
+    "Image" : {
+      "Name" : "${aws_ecr_repository.app.repository_url}:latest",
+      "Update" : "true"
+    },
+    "Ports" : [
+      {
+        "ContainerPort" : "8000",
+        "HostPort" : "8000"
+      }
+    ],
+    "Entrypoint" : "/root/app",
+    "Command" : "arg"
+
+
+  })
   lifecycle {
     replace_triggered_by = [local_file.ebs_config]
   }
